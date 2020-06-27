989fee59a96b8141b2ead769d03c6df709b206a7
Benjamin-Pasternak <bpastmybear@gmail.com>
2020-06-27 16:56:02 -0400

    added manhattan distance calc

--- project/A*.py
+++ project/A*.py
@@ -5,12 +5,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+
+# this function computes the manhattan distance given 2 tuples (x,y)
+# ex:
+# xy1 = (2, 2)
+# xy2 = (4, 4)
+# yields 4
+def manhattan_distance(xy1, xy2):
+    return abs(xy1[0] - xy2[0]) + abs(xy1[1] - xy2[1])
+
+def compute_path(arr):
+
+
+
+# note heapq should be sorted based on f which is g + h or step cost + heuristic (manhattan distance)
+# test stuff
+xy1 = (2, 2)
+xy2 = (4, 4)
+print(manhattan_distance(xy1, xy2))
+print(float('inf'))
 col = 5
 row = 5
 # open set = heapq
-#closed set = visited
+# closed set = visited
 grid = np.ones((row, col), dtype=bool)
-print(grid)
+
+
+# print(grid)
 
 
 class Node:
@@ -18,4 +39,3 @@ class Node:
         self.g = 0
         self.h = 0
         self.f = 0
-
